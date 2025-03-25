fb1e9718b71e965c1510f26116d347bfc2519e09
Eralo <115709869+Eralo@users.noreply.github.com>
2025-03-25 14:53:18 +0100

    initialize project, header and footer components

---
+++ src/components/SiteFooter.js
@@ -0,0 +1,43 @@
+/* TODO :
+
+- MAKE hamb. menu with all elements
+- LINK cart service
+- LINK all menu items on hamb. menu
+- CHANGE logo from text to image
+- REVIEW behaviour (collapsible, etc.)
+- REVIEW & MAKE search bar
+
+*/
+
+import React from "react";
+import './SiteFooter.css';
+import { FaSquareFacebook, FaSquareTwitter, FaInstagram } from "react-icons/fa6"; // https://react-icons.github.io/react-icons/
+import { IoLanguageOutline } from "react-icons/io5";
+import bannerLogo from '../Resources/CESIEat_BannerLogo.png';
+
+
+function SiteFooter() {
+    return (
+        <div className="footerContainer">
+            <img src={bannerLogo} className="bannerLogo" alt="bannerLogo"/>
+            <div className="footLinks">
+                <div className="footIcons">
+                    <FaSquareFacebook className="footIcon"/>
+                    <FaSquareTwitter className="footIcon"/>
+                    <FaInstagram className="footIcon"/>
+                    <div className="footLanguageIcon">
+                        <IoLanguageOutline className="footIcon"/> 
+                        <p> Francais </p>
+                    </div>
+                </div>
+                <div className="footTexts">
+                    <a href="../public/index.html"> Politique de confidentialité </a>
+                    <a href="../public/index.html"> Conditions d'utilisation </a>
+                    <a href="../public/index.html"> Mes informations personnelles </a>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default SiteFooter;
