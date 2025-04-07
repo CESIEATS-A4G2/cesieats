155467ff6098143ef3526472d46859dc877dde64
EthanTESTA <testaethan@gmail.com>
2025-04-07 14:06:46 +0200

    108: ajout actions sur l'api order

--- api/src/routes/orderRoutes.ts
+++ api/src/routes/orderRoutes.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createOrder, getOrdersByAccountId, getOrdersByAccountIdByStatus, getOrdersByStatus } from '../controllers/orderController';
+import { createOrder, getOrderById, getOrdersByAccountId, getOrdersByAccountIdByStatus, getOrdersByStatus, updateOrderStatus } from '../controllers/orderController';
 
 const router = express.Router();
 
@@ -7,6 +7,7 @@ router.post('/:account_id/orders', createOrder);
 router.get('/:account_id/orders', getOrdersByAccountId);
 router.get('/:account_id/orders/status', getOrdersByAccountIdByStatus);
 router.get('/orders/status', getOrdersByStatus);
-
+router.put('/:account_id/orders/:order_id', updateOrderStatus);
+router.get('/:account_id/orders/:order_id', getOrderById);
 
 export default router;
